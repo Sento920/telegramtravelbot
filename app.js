836dd94e4bb8880d119a70cb992557a6ea390372
Sento920 <sentokun9200@hotmail.com>
2020-10-25 23:59:41 -0500

    Added Promise handling for Incoming Data, outputs to file as JSON structure.

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 const TelegramBot = require('node-telegram-bot-api');
 const wiki = require('wikijs').default;
 var config = require('./config');
-var parseInfo = require("infobox-parser");
+var fs = require('fs');
 
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = config.telegrambotid;
 const bot = new TelegramBot(token, {polling: true});
+var msgId = '0';
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
   const chatId = config.chatid;
+  msgId = msg.chat.id; // Set global Telegram ID we can pass along the data process.
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Input Received, ' + msg.chat.first_name + '.');
   if(msg.chat.id = config.chatid){
     bot.sendMessage(chatId, 'Processing Input, looking for : \"' + msg.text + '\".');
-    ProcessData(msg.text);
+    GatherData(msg.text, msgId);
   } else {
-    bot.sendMessage(msg.chat.id, "Identity not verified by developer. Ignoring Request.");
+    bot.sendMessage(msg.chat.id, 'Identity not verified by developer. Ignoring Request.');
   }
-
 });
 
 
-function ProcessData(data){ // Here will be where we parse the data and hand off to the wikipedia library.
-  console.log('data: ' + data);
-  wiki()
-    .page(data)
-    .then(page => page.info('alterEgo'))
-    .then(console.log); // Bruce Wayne
+function GatherData(input, msgId){ // Here will be where we parse the data and hand off to the wikipedia library.
+  console.log('\nIncoming data: ' + input);
+  wiki().page(input).then(
+    function resolve(page){ //RESOLVE: Process Data.
+      var promises = [];
+      promises.push(page.summary());
+      promises.push(page.content());
+      promises.push(page.coordinates());
+      Promise.all(promises).then(values => {
+        fs.writeFile('out.txt', JSON.stringify(values), function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+        });
+      });
+    }, 
+    function reject(){ // Reject: We spit the user an error message, and let them handle it.
+      console.log('Rejected');
+      bot.sendMessage(msgId, 'We didn\'t find an article for ' + input + '. Please Try a new search. ');
+    });
+
 }
 
